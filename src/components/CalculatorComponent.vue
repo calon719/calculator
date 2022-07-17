70b649d4dc788cf87520a6f783e484d0d755b0af
Calon <calonhu@protonmail.com>
2022-07-17 12:14:18 +0800

    新增計算結果、倒退字串、顯示前一筆結果功能

--- src/components/CalculatorComponent.vue
+++ src/components/CalculatorComponent.vue
@@ -2,14 +2,14 @@
 export default {
   data() {
     return {
-      calString: '0',
       numString: '0',
       stringArr: [],
       history: '0',
+      showAnswer: false,
     };
   },
-  watch: {
-    numString() {
+  computed: {
+    displayString() {
       const formattedStringArr = this.stringArr.map((string) => {
         let formattedString = '';
         if (string === '/') {
@@ -21,11 +21,14 @@ export default {
         }
         return formattedString;
       });
-      this.calString = formattedStringArr.join('') + this.numString;
+      return formattedStringArr.join('') + this.numString;
     },
   },
   methods: {
     inputNumString(string) {
+      if (this.showAnswer) {
+        this.init();
+      }
       if (this.numString === '0' && (string === '0' || string === '00')) {
         this.numString = '0';
       } else if (this.numString === '0' && string !== '.') {
@@ -36,15 +39,37 @@ export default {
         this.numString += string;
       }
     },
-    saveNum(operator) {
+    setOperation(operation) {
+      if (this.showAnswer) {
+        this.showAnswer = false;
+      }
       if (this.numString !== '') {
-        this.stringArr.push(this.numString, operator);
+        this.stringArr.push(this.numString, operation);
         this.numString = '';
       }
     },
     init() {
+      this.showAnswer = false;
       this.stringArr.splice(0);
       this.numString = '0';
+    },
+    deleteString() {
+      if (this.numString === '' && this.stringArr.length !== 0) {
+        this.stringArr.splice(-1, 1);
+        [this.numString] = this.stringArr.splice(-1, 1);
+      } else {
+        this.numString = this.numString.slice(0, -1);
+        if (this.numString.length <= 0) {
+          this.numString = '0';
+        }
+      }
+    },
+    computeNum() {
+      const answer = eval(this.stringArr.join('') + this.numString);
+      this.history = `${this.displayString}=${answer}`;
+      this.numString = answer;
+      this.stringArr.splice(0);
+      this.showAnswer = true;
     },
   },
 };
@@ -53,82 +78,18 @@ export default {
 <template>
   <div class="rounded-lg p-8 mx-auto bg-zinc-800 w-80 drop-shadow-2xl">
     <div class="rounded-lg bg-zinc-300 px-4 py-2 mb-4">
-      <div class="text-right overflow-hidden flex flex-col justify-end">
-        <p class="text-sm text-gray-500">
+      <div class="text-right overflow-hidden relative">
+        <p class="text-sm text-gray-500 mb-6">
           {{ history }}
         </p>
-        <p>
-          {{ calString }}
+        <p class="whitespace-nowrap absolute bottom-0 right-0">
+          {{ displayString }}
         </p>
-      </div>
-    </div>
-    <div class="grid grid-cols-12 gap-4 text-zinc-300 mb-4">
-      <div class="col-span-9 grid grid-cols-3 gap-4">
-        <button
-          v-for="num in 9"
-          :key="'cal' + num"
-          class="w-full py-1 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="inputNumString(String(num))"
-        >
-          {{ num }}
-        </button>
-        <button
-          class="w-full py-1 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="inputNumString('0')"
-        >
-          0
-        </button>
-        <button
-          class="w-full py-1 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="inputNumString('00')"
-        >
-          00
-        </button>
-        <button
-          class="w-full py-1 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="inputNumString('.')"
-        >
-          .
-        </button>
-      </div>
-      <div class="col-span-3">
-        <button
-          class="mb-4 bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="saveNum('/')"
-        >
-          ÷
-        </button>
-        <button
-          class="mb-4 bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="saveNum('*')"
-        >
-          ×
-        </button>
-        <button
-          class="mb-4 bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="saveNum('-')"
-        >
-          －
-        </button>
-        <button
-          class="bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
-          type="button"
-          @click="saveNum('+')"
-        >
-          ＋
-        </button>
       </div>
     </div>
     <div class="grid grid-cols-4 gap-4 text-zinc-300">
       <button
-        class="bg-red-600 py-4 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        class="col-span-2 bg-red-600 py-4 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
         type="button"
         @click="init"
       >
@@ -137,13 +98,120 @@ export default {
       <button
         class="bg-red-600 py-4 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
         type="button"
+        @click="deleteString"
       >
         <i class="bi bi-arrow-return-left" />
+      </button>
+      <button
+        class="bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="setOperation('/')"
+      >
+        ÷
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('1')"
+      >
+        1
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('2')"
+      >
+        2
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('3')"
+      >
+        3
+      </button>
+      <button
+        class="bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="setOperation('*')"
+      >
+        ×
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('4')"
+      >
+        4
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('5')"
+      >
+        5
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('6')"
+      >
+        6
+      </button>
+      <button
+        class="bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="setOperation('-')"
+      >
+        －
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('7')"
+      >
+        7
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('8')"
+      >
+        8
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('9')"
+      >
+        9
+      </button>
+      <button
+        class="bg-red-600 py-4 rounded-lg text-center w-full hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="setOperation('+')"
+      >
+        ＋
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('.')"
+      >
+        .
+      </button>
+      <button
+        class="w-full py-4 bg-purple-800 text-center rounded-lg hover:opacity-70 drop-shadow-lg"
+        type="button"
+        @click="inputNumString('0')"
+      >
+        0
       </button>
       <button
         class="col-span-2 bg-amber-600 p-4 w-full text-center rounded-lg
         hover:opacity-70 drop-shadow-lg"
         type="button"
+        @click="computeNum"
       >
         =
       </button>
