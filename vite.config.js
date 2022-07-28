31312e652a097d472eb06e624e1c437f50eea587
Calon <calonhu@protonmail.com>
2022-07-28 10:48:02 +0800

    修改 vite.config.js 部屬路徑

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://calon719.github.io/calculator/',
+  base: '/calculator/',
 });
